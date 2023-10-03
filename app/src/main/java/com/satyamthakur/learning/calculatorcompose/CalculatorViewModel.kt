44602add6d30401712079a77c4ebe21901b6d8b2
Satyam Thakur <sat.thakur20@gmail.com>
2023-10-03 11:02:54 +0530

    Implementing business logic with performDeletion and enterOperation funcitons in ViewModel

--- app/src/main/java/com/satyamthakur/learning/calculatorcompose/CalculatorViewModel.kt
+++ app/src/main/java/com/satyamthakur/learning/calculatorcompose/CalculatorViewModel.kt
@@ -8,4 +8,47 @@ import androidx.lifecycle.ViewModel
 class CalculatorViewModel: ViewModel() {
     var state by mutableStateOf(CalculatorState())
         private set
+
+    fun onAction(action: CalculatorAction) {
+        when(action) {
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Clear -> state = CalculatorState()
+            is CalculatorAction.Operation -> enterOperation(action.operation)
+            is CalculatorAction.Calculate -> performCalculation()
+            is CalculatorAction.Delete -> performDeletion()
+        }
+    }
+
+    private fun performDeletion() {
+        when {
+            state.number2.isNotBlank() -> state = state.copy(
+                number2 = state.number2.dropLast(1)
+            )
+            state.operation != null -> state = state.copy(
+                operation = null
+            )
+            state.number1.isNotBlank() -> state = state.copy(
+                number1 = state.number1.dropLast(1)
+            )
+        }
+    }
+
+    private fun performCalculation() {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+        if (state.number1.isNotBlank()) {
+            state = state.copy(operation = operation)
+        }
+    }
+
+    private fun enterDecimal() {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterNumber(number: Int) {
+        TODO("Not yet implemented")
+    }
 }
